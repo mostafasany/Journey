b9b044c94a453a3b4df8112b0cdb922c23efa958
mostafa sany saad khodeir <mostafasany.khodeir@hotmail.com>
2018-02-07 18:21:28 +0100

    Fix Image Issue in Windows UWP

--- Journey/Journey/Views/Controls/ImageControl.cs
+++ Journey/Journey/Views/Controls/ImageControl.cs
@@ -1,26 +1,35 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Abstractions.Exceptions;
+using Xamarin.Forms;
 
 namespace Journey.Views.Controls
 {
     public class ImageControl : Image
     {
         public static readonly BindableProperty PortablePathProperty =
-            BindableProperty.Create(nameof(PortablePath), typeof(string), typeof(ImageControl),null,
+            BindableProperty.Create(nameof(PortablePath), typeof(string), typeof(ImageControl), null,
         BindingMode.TwoWay,
         null,
         (bindable, oldValue, newValue) =>
         {
             var val = newValue.ToString();
-            (bindable as ImageControl).Source = ImageSource.FromResource(val);
+            try
+            {
+                (bindable as ImageControl).Source = Device.RuntimePlatform == Device.UWP ? ImageSource.FromFile(val.Replace("Journey.Assets.", "")) : ImageSource.FromResource(val);
+            }
+            catch (Exception e)
+            {
+            }
+
         });
 
         public string PortablePath
         {
             get { return (string)GetValue(PortablePathProperty); }
             set
-            { 
+            {
                 SetValue(PortablePathProperty, value);
-               
+
             }
         }
     }
