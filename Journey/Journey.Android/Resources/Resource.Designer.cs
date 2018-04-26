0694c7ca7c02738af56fae0ef679305bde2fa264
mostafasany <mostafasany.khodeir@hotmail.com>
2018-04-26 20:57:36 +0200

    commit

--- Journey/Journey.Android/Resources/Resource.Designer.cs
+++ Journey/Journey.Android/Resources/Resource.Designer.cs
@@ -2259,26 +2259,8 @@ namespace Journey.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020126
-			public const int avd_hide_password_1 = 2130837798;
-			
-			// aapt resource value: 0x7f020127
-			public const int avd_hide_password_2 = 2130837799;
-			
-			// aapt resource value: 0x7f020128
-			public const int avd_hide_password_3 = 2130837800;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
-			
-			// aapt resource value: 0x7f020129
-			public const int avd_show_password_1 = 2130837801;
-			
-			// aapt resource value: 0x7f02012a
-			public const int avd_show_password_2 = 2130837802;
-			
-			// aapt resource value: 0x7f02012b
-			public const int avd_show_password_3 = 2130837803;
 			
 			// aapt resource value: 0x7f020055
 			public const int common_full_open_on_phone = 2130837589;
